04ee2959f139b98c3eec0ed81c145c4f0aac58e4
pawan prajaparti <pawanp226@gmail.com>
2020-05-31 12:37:42 +0530

    Add hello msg to infinit for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld{
 public static void main(String []args){
+
+for(int i=0;ture;i++){
 System.out.println("Hello pawan");
-System.out.println("Hello Indore");
-System.out.println("Hello From my own Branch");
+}
+
 }
 }
